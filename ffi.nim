fc743fbbdd2423d6f541e78cbd5c278cac8137ce
Ivan Folgueira Bande <ivansete@status.im>
2025-08-16 02:14:29 +0200

    more positive progress. Getting closer

--- ffi.nim
+++ ffi.nim
@@ -1,3 +1,5 @@
-import ffi/alloc
+import
+  ffi/[alloc, ffi_types, ffi_context, ffi_thread_request],
+  ffi/internal/[ffi_library, ffi_macro]
 
-export alloc
+export alloc, ffi_library, ffi_macro, ffi_types, ffi_context, ffi_thread_request
--- ffi/internal/ffi_library.nim
+++ ffi/internal/ffi_library.nim
@@ -1,51 +1,82 @@
-import std/macros, strformat
+import std/[macros, atomics], strformat, chronicles, chronos
 
 macro declareLibrary*(libraryName: static[string]): untyped =
-  result = newStmtList()
+  var res = newStmtList()
 
-  {.pragma: exported, exportc, cdecl, raises: [].}
-  {.pragma: callback, cdecl, raises: [], gcsafe.}
-  {.passc: "-fPIC".}
+  ## Generate {.pragma: exported, exportc, cdecl, raises: [].}
+  res.add nnkPragma.newTree(
+    nnkExprColonExpr.newTree(ident"pragma", ident"exported"),
+    ident"exportc",
+    ident"cdecl",
+    nnkExprColonExpr.newTree(ident"raises", nnkBracket.newTree()),
+  )
+
+  ## Generate {.pragma: callback, cdecl, raises: [], gcsafe.}
+  res.add nnkPragma.newTree(
+    nnkExprColonExpr.newTree(ident"pragma", ident"callback"),
+    ident"cdecl",
+    nnkExprColonExpr.newTree(ident"raises", nnkBracket.newTree()),
+    ident"gcsafe",
+  )
+
+  ## Generate {.passc: "-fPIC".}
+  res.add nnkPragma.newTree(nnkExprColonExpr.newTree(ident"passc", newLit("-fPIC")))
 
   when defined(linux):
-    ## Generates {.passl: "-Wl,-soname,libwaku.so".}
+    ## Generates {.passl: "-Wl,-soname,libwaku.so".} (considering libraryName=="waku", for example)
     let soName = fmt"-Wl,-soname,lib{libraryName}.so"
-    result.add(nnkPragmaStmt.newTree(ident"passl", newStrLitNode(soName)))
+    res.add(
+      newNimNode(nnkPragma).add(
+        nnkExprColonExpr.newTree(ident"passl", newStrLitNode(soName))
+      )
+    )
 
   ## proc lib{libraryName}NimMain() {.importc.}
-  let procName = ident(fmt"lib{libraryName}NimMain")
+  let libNimMainName = ident(fmt"lib{libraryName}NimMain")
   let importcPragma = nnkPragma.newTree(ident"importc")
   let procDef = newProc(
-    name = procName,
-    params = @[],
+    name = libNimMainName,
+    params = @[ident"void"],
     pragmas = importcPragma,
     body = newEmptyNode(),
-    returnType = newEmptyNode(), # no return value
   )
-  result.add(procDef)
+  res.add(procDef)
 
-################################################################################
-### Library setup
+  # Create: var initialized: Atomic[bool]
+  let atomicType = nnkBracketExpr.newTree(ident("Atomic"), ident("bool"))
+  let varStmt = nnkVarSection.newTree(
+    nnkIdentDefs.newTree(ident("initialized"), atomicType, newEmptyNode())
+  )
+  res.add(varStmt)
 
-# To control when the library has been initialized
-var initialized: Atomic[bool]
+  ## Android chronicles redirection
+  let chroniclesBlock = quote:
+    when defined(android) and compiles(defaultChroniclesStream.outputs[0].writer):
+      defaultChroniclesStream.outputs[0].writer = proc(
+          logLevel: LogLevel, msg: LogOutputStr
+      ) {.raises: [].} =
+        echo logLevel, msg
+      result.add(chroniclesBlock)
 
-if defined(android):
-  # Redirect chronicles to Android System logs
-  when compiles(defaultChroniclesStream.outputs[0].writer):
-    defaultChroniclesStream.outputs[0].writer = proc(
-        logLevel: LogLevel, msg: LogOutputStr
-    ) {.raises: [].} =
-      echo logLevel, msg
+  let procName = ident("initializeLibrary")
+  let nimMainName = ident("lib" & libraryName & "NimMain")
 
-proc initializeLibrary() {.exported.} =
-  if not initialized.exchange(true):
-    ## Every Nim library needs to call `<yourprefix>NimMain` once exactly, to initialize the Nim runtime.
-    ## Being `<yourprefix>` the value given in the optional compilation flag --nimMainPrefix:yourprefix
-    libwakuNimMain()
-  when declared(setupForeignThreadGc):
-    setupForeignThreadGc()
-  when declared(nimGC_setStackBottom):
-    var locals {.volatile, noinit.}: pointer
-    locals = addr(locals)
-    nimGC_setStackBottom(locals)
+  let initializeLibraryProc = quote:
+    proc `procName`() {.exported.} =
+      if not initialized.exchange(true):
+        ## Every Nim library needs to call `<yourprefix>NimMain` once exactly,
+        ## to initialize the Nim runtime.
+        ## Being `<yourprefix>` the value given in the optional
+        ## compilation flag --nimMainPrefix:yourprefix
+        `nimMainName`()
+      when declared(setupForeignThreadGc):
+        setupForeignThreadGc()
+      when declared(nimGC_setStackBottom):
+        var locals {.volatile, noinit.}: pointer
+        locals = addr(locals)
+        nimGC_setStackBottom(locals)
+
+  res.add(initializeLibraryProc)
+
+  echo result.repr
+  return res
